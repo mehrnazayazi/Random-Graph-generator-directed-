4ef727bd7b5dbd6fe94aae193eb7c7b99b5616b3
mehrnaz <mehrnaz1995@gmail.com>
2020-01-08 13:54:50 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Random_Graph_Generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Random_Graph_Generator.dir/main.cpp.o"
+  "Random_Graph_Generator"
+  "Random_Graph_Generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Random_Graph_Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Random_Graph_Generator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Random_Graph_Generator.
+# This may be replaced when dependencies are built.
