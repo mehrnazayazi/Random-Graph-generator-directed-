377b161091f66a8ccb6da6f44b7601fb906a575a
mehrnaz <mehrnaz1995@gmail.com>
2020-01-08 14:17:49 +0800

    represent two matrixs of randomly filled(cost and capacity)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numV, cost[250][250], cap[250][250];
+    for (int j = 0; j < 250; ++j) {
+        for (int i = 0; i < ; ++i) {
+            cost[i][j] = -1;
+            cap[i][j] = -1;
+        }
+    }
+    numV = rand() % 250;// v1 in the range 0 to 99
+    for (int i = 0; i < numV ; ++i) {
+        int iteration = rand() % numV;
+        for (int j = 0; j < iteration; ++j) {
+            int Vnum = rand() % numV;
+            if(cost[i][Vnum]==-1 && cost[Vnum][i]==-1){
+                int Ecost = rand() % 2000;
+                int Ecap = rand() % 2000;
+                cost[i][Vnum] = Ecost;
+                cap[i][Vnum] = Ecap;
+            }
+        }
+    }
     return 0;
 }
